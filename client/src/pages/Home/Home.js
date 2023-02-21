4abb70c44511714fc160e40c411783edc03b57f6
andrewHotyun <kapitansoup200047@gmail.com>
2023-02-21 15:42:11 +0200

    minorFixes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -64,7 +64,7 @@ const Home = (props) => {
                     Our unique approach allows you to receive an unmatched breadth of business
                     name ideas from world's largest community of naming experts.
                     With 75,000+ creatives and 15,000+ successful naming projects,
-                    Squadhelp is by far the largest naming platform across the globe .
+                    Squadhelp is by far the largest naming platform across the globe.
 </p>
                 </div>
                 <div className={styles.card}>
